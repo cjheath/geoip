712206ff3114f73b2f661bb0e6fb3f32f782b720
Clifford Heath <clifford.heath@gmail.com>
2014-03-26 11:40:06 +1100

    Rolled version to 1.4.0 for the preload behaviour

--- lib/geoip.rb
+++ lib/geoip.rb
@@ -58,7 +58,7 @@ require 'yaml'
 class GeoIP
 
   # The GeoIP GEM version number
-  VERSION = "1.3.5"
+  VERSION = "1.4.0"
 
   # The +data/+ directory for geoip
   DATA_DIR = File.expand_path(File.join(File.dirname(__FILE__),'..','data','geoip'))
