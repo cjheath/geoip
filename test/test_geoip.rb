8d183d8328273378fd342a8ea9a21696a30a5d3b
Ken Dreyer <ktdreyer@ktdreyer.com>
2014-04-03 15:35:54 -0500

    tests: add simple constructor test
    Create a new GeoIP object, and verify that it's an instance of GeoIP.
    
    If GeoIP.dat is unavailable, skip the test.

--- test/test_geoip.rb
+++ test/test_geoip.rb
@@ -3,9 +3,16 @@ require File.dirname(__FILE__) + '/test_helper.rb'
 class TestGeoip < Minitest::Test
 
   def setup
+    data = '/usr/share/GeoIP/GeoIP.dat'
+    begin
+      @g = GeoIP.new(data)
+    rescue Errno::ENOENT => e
+      skip e.message
+    end
   end
 
-  def test_truth
-    assert true
+  def test_constructor
+    assert_instance_of GeoIP, @g
   end
+
 end
