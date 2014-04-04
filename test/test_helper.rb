45a5dd5b449ae09f9c02c36018bf9acae2577f19
Ken Dreyer <ktdreyer@ktdreyer.com>
2014-04-04 10:33:36 -0500

    tests: add support for Minitest versions < 5
    Minitest versions 4 and below do not support the newer Minitest::Test
    class that arrived in version 5. Use MiniTest::Unit::TestCase as a
    fallback.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,10 @@
 require 'stringio'
 require 'minitest/autorun'
 require File.dirname(__FILE__) + '/../lib/geoip'
+
+if Minitest.const_defined?('Test')
+  # We're on Minitest 5+. Nothing to do here.
+else
+  # Minitest 4 doesn't have Minitest::Test yet.
+  Minitest::Test = MiniTest::Unit::TestCase
+end
